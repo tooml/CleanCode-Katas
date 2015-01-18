2d4f0da6c94d4543c18ae359de8db3e9251f523d
tooml <toom-l@hotmail.de>
2015-01-18 19:09:50 +0100

    Creation

---
+++ RömischeZahlenKata/RömischeZahlenConsole/Program.cs
@@ -0,0 +1,21 @@
+﻿using RömischeZahlen;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RömischeZahlenConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DezimalzahlenWandler test = new DezimalzahlenWandler();
+            var zahl = test.ParseRömischeZahlInDezimal("MDCLXXIX");
+            Console.WriteLine(zahl);
+
+            Console.ReadKey();
+        }
+    }
+}
